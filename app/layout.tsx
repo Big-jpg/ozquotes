5dd9198a884e6998ef03485327d6680ba8d78d05
Ross Farrell <rossf@activeapparel.com.au>
2024-10-04 17:50:05 +0800

    modified:   README.md 	modified:   app/favicon.ico 	modified:   app/layout.tsx 	new file:   public/images/ogImageHeader.png

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css';
 import Navbar from '@/components/ui/navbar';
 import { ReactNode } from 'react';
+import Head from 'next/head';
 
 // Define the type for children prop
 interface RootLayoutProps {
@@ -10,6 +11,16 @@ interface RootLayoutProps {
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en">
+      <Head>
+        {/* Global Open Graph Meta Tags */}
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="description" content="Discover the best energy upgrade quotes, powered by AI and advanced analytics." />
+        <meta property="og:title" content="Oz Quotes - Get the Best Energy Upgrade Quotes" />
+        <meta property="og:description" content="Get the Best Energy Upgrade Quotes with AI-Powered Insights and Exclusive Rewards" />
+        <meta property="og:image" content="/images/ogImageHeader.png" />
+        <meta property="og:url" content="https://ozquotes.com" />
+        <meta property="og:type" content="website" />
+      </Head>
       <body>
         <Navbar />
         {children}
